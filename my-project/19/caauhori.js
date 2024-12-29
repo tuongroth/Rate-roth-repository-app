7b29751cf5923553414b0d0b86c039663f60de81
TuongRoth <rothtrancattuong0703@gmail.com>
2024-12-29 16:50:08 +0200

    192 cà 127 là lặp

--- my-project/19/caauhori.js
+++ my-project/19/caauhori.js
@@ -1,1 +1,52 @@
 sự khác biệt có công cụ và k có: mất trắng
+có đẩy lùi được bóng đen k 
+có local k 
+có điểm cuối k 
+có danh sách phân trang KeyboardEvent
+Sau khi đột biến createReview thành công , 
+có ng dùng mới k 
+nếu ko có parsePath, đoạn đường đó m kbh hoàn thành decodeURI
+trigger provoke or Notification
+
+nhớ lại coi : slow enough that can i survive til the end?
+cô ấy cướp những thứ t thích để trả thù và khiến ng khác nghĩ cô ấy là nạn nhân 
+t k thích làm nạn nhân, thích hung thủ cơ 
+
+phe đứng đắn, hết trễ là được chứ đúng sai cái gì 
+nhớ lại coi: có giúp cũng k dc tích sự gì 
+nhớ lại coi: mấy con mẹ chúa trời này vừa ăn cắp vừa la làng lại tưởng mình đẹp nhất 
+nhớ lại coi: cũng chính mấy ng cung cấp thông tin lệch lạc khiến ng khác hiểu nhầm lajlaafn, tới pp cũng vậy , t k phải cầu nối cho ba về cpc đâu, t là cầu nối về với cha t 
+no small talkbetter, because cold less energies
+vn t sợ ma,plan t sợ ng hơn sợ ma , mental health, 
+
+hèn gì họ đisauna, 
+
+có những ng họ chết r nhưng k nhận RadioNodeList, nên t tình nguyện dẫn đường để họ về nhà 
+có một mô hình khi về VN: chất lượng đâu ko thấy, thấy bùi viện, thấy casino cò mồi dắt , thấy đa cấp tìm việc làm thêm , 
+
+qa pl: imre , cạy mỏ k khai, k dc tới đó, 
+1 ng kì thôi, k thể 2 ng
+
+ý niệm: academy k thể thua dc, they talk about dominate, ênrgies
+
+i enter to new home space || are you refering 
+nhớ lại đi: vị trí ngang nhau nhưng ng này đẩy t vô đường cùng, ng kia đưa t ts bác sĩ 
+nhớ lại đi: therapy nhưng instanceof, ng thì học finn đi 
+cùng kết quả nhưng k dc tích sự gì, chỉ tổ làm t điên thêm r, 
+nhớ lại đi: nếu k muốn nghe giải thích thì thích cc gì, m là nạn nhân, t là hung thủ k thấy s , cuộc đời 1 ng vô tội ko làm gì chỉ vì mấy đứa như mày mà hại ng khác mất nhà mất cửa, để t coi 1 lời nói có chạm đến đất đai ko 
+you said feedback not important, nhưng feedback làm pp xém nửa giết t đó , trong khi t ms là ng bị giết t ho ra máu nhưng t k dám gặp pp vì t sợ a ta quýnh t 
+nhiều khói quá má, vision ahead
+thoát khỏi hàng chờ 
+chậm chạp, 2 day thôi 
+
+my boss proof how Weird 
+you save my name daddy, k ưa
+hi, hi, so i have to comeback, yes
+so tonight you dontwanna come, no
+
+t chết ở đâu kệ t lqan j đến a 
+to help me make a decision, create application , 
+
+s nó khớp vl 
+
+phá băng 
